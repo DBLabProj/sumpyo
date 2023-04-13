6aa7d15362dcb2dfcd582a3a165ceacbca1bb38c
Platform1117 <230233@naver.com>
2023-04-13 23:32:29 +0900

    Calendar Design

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:sumpyo/screens/home_screen.dart';
 
-void main() {
+void main() async {
+  await initializeDateFormatting();
   runApp(const App());
 }
 
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,42 +14,51 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: topAppBar(
+        appBar: AppBar(),
+      ),
       bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.grey,
         backgroundColor: Colors.white,
         currentIndex: current_index,
+        type: BottomNavigationBarType.fixed,
         onTap: (index) {
           print('index test : $index');
           setState(() {
             current_index = index;
           });
         },
-        items: const [
+        items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(
               Icons.calendar_month_outlined,
-              color: Color(0xFF8AA9DE),
             ),
             label: 'Calendar',
           ),
           BottomNavigationBarItem(
             icon: Icon(
               Icons.insert_drive_file_outlined,
-              color: Color(0xFF8AA9DE),
             ),
             label: 'Clip',
           ),
           BottomNavigationBarItem(
             icon: Icon(
+              Icons.circle,
+              size: 30,
+            ),
+            label: 'Somthing',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
               Icons.bar_chart_rounded,
-              color: Color(0xFF8AA9DE),
             ),
             label: 'Report',
           ),
           BottomNavigationBarItem(
             icon: Icon(
               Icons.supervised_user_circle,
-              color: Color(0xFF8AA9DE),
             ),
             label: 'MyPage',
           ),
@@ -56,9 +66,102 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Column(
         children: const [
-          Text('Data'),
+          mainCalendar(),
         ],
       ),
+    );
+  }
+}
+
+class topAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final AppBar appBar;
+  const topAppBar({super.key, required this.appBar});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu_rounded,
+            size: 40,
+          ),
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.account_circle_rounded,
+                size: 40,
+              ))
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(appBar.preferredSize.height);
+}
+
+class mainCalendar extends StatefulWidget {
+  const mainCalendar({super.key});
+
+  @override
+  State<mainCalendar> createState() => _mainCalendarState();
+}
+
+class _mainCalendarState extends State<mainCalendar> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime? selectedDay;
+  DateTime focusedDay = DateTime.now();
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      locale: 'ko_KR',
+      focusedDay: focusedDay,
+      firstDay: DateTime.utc(2000, 1, 1),
+      lastDay: DateTime.utc(2050, 12, 31),
+      calendarFormat: _calendarFormat,
+      selectedDayPredicate: (day) {
+        return isSameDay(selectedDay, day);
+      },
+      headerStyle: HeaderStyle(
+        headerPadding: EdgeInsets.symmetric(
+            vertical: MediaQuery.of(context).size.height * 0.03,
+            horizontal: MediaQuery.of(context).size.width * 0.05),
+        leftChevronVisible: false,
+        rightChevronVisible: false,
+        formatButtonVisible: false,
+      ),
+      calendarStyle: CalendarStyle(
+        todayDecoration: const BoxDecoration(
+          color: Color(0xff5A6E90),
+          shape: BoxShape.circle,
+        ),
+        selectedDecoration: BoxDecoration(
+          color: Colors.transparent,
+          border: Border.all(color: const Color(0xFFB9E1EC), width: 3),
+          shape: BoxShape.circle,
+          // border: Border.all(color: const Color(0xFFB9E1EC), width: 3),
+        ),
+        defaultTextStyle: const TextStyle(color: Colors.white),
+        weekendTextStyle: const TextStyle(color: Colors.white),
+        outsideTextStyle: TextStyle(
+          color: Colors.white.withOpacity(0.4),
+        ),
+      ),
+      onDaySelected: (selectedDay, focusedDay) {
+        setState(() {
+          _calendarFormat = CalendarFormat.week;
+          this.selectedDay = selectedDay;
+          this.focusedDay = focusedDay;
+        });
+      },
     );
   }
 }
