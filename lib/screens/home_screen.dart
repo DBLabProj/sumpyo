4804bcc4bf6d22db78412107808ce5d68a7476be
Platform1117 <230233@naver.com>
2023-04-15 13:28:30 +0900

    add sliding panel

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel2/sliding_up_panel2.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,8 +10,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int current_index = 0;
-
+  CalendarFormat calFormat = CalendarFormat.month;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,56 +18,33 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: topAppBar(
         appBar: AppBar(),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Theme.of(context).primaryColor,
-        unselectedItemColor: Colors.grey,
-        backgroundColor: Colors.white,
-        currentIndex: current_index,
-        type: BottomNavigationBarType.fixed,
-        onTap: (index) {
-          print('index test : $index');
-          setState(() {
-            current_index = index;
-          });
+      bottomNavigationBar: const bottomNav(),
+      body: SlidingUpPanel(
+        onPanelOpened: () {
+          calFormat = CalendarFormat.week;
+          setState(() {});
         },
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.calendar_month_outlined,
-            ),
-            label: 'Calendar',
+        borderRadius: const BorderRadius.vertical(top: Radius.circular(25)),
+        panelBuilder: () {
+          return const diaryContainer();
+        },
+        body: GestureDetector(
+          onDoubleTap: () {
+            print('double Taped');
+            setState(
+              () {
+                calFormat = CalendarFormat.month;
+              },
+            );
+          },
+          child: Column(
+            children: [
+              mainCalendar(
+                calendarFormat: calFormat,
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.insert_drive_file_outlined,
-            ),
-            label: 'Clip',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.circle,
-              size: 30,
-            ),
-            label: 'Somthing',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.bar_chart_rounded,
-            ),
-            label: 'Report',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.supervised_user_circle,
-            ),
-            label: 'MyPage',
-          ),
-        ],
-      ),
-      body: Column(
-        children: const [
-          mainCalendar(),
-        ],
+        ),
       ),
     );
   }
@@ -109,14 +86,17 @@ class topAppBar extends StatelessWidget implements PreferredSizeWidget {
 }
 
 class mainCalendar extends StatefulWidget {
-  const mainCalendar({super.key});
+  CalendarFormat calendarFormat;
+  mainCalendar({
+    super.key,
+    required this.calendarFormat,
+  });
 
   @override
   State<mainCalendar> createState() => _mainCalendarState();
 }
 
 class _mainCalendarState extends State<mainCalendar> {
-  CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime? selectedDay;
   DateTime focusedDay = DateTime.now();
   @override
@@ -126,7 +106,7 @@ class _mainCalendarState extends State<mainCalendar> {
       focusedDay: focusedDay,
       firstDay: DateTime.utc(2000, 1, 1),
       lastDay: DateTime.utc(2050, 12, 31),
-      calendarFormat: _calendarFormat,
+      calendarFormat: widget.calendarFormat,
       selectedDayPredicate: (day) {
         return isSameDay(selectedDay, day);
       },
@@ -157,11 +137,114 @@ class _mainCalendarState extends State<mainCalendar> {
       ),
       onDaySelected: (selectedDay, focusedDay) {
         setState(() {
-          _calendarFormat = CalendarFormat.week;
+          widget.calendarFormat = CalendarFormat.week;
           this.selectedDay = selectedDay;
           this.focusedDay = focusedDay;
         });
       },
+    );
+  }
+}
+
+class diaryContainer extends StatefulWidget {
+  const diaryContainer({super.key});
+
+  @override
+  State<diaryContainer> createState() => _diaryContainerState();
+}
+
+class _diaryContainerState extends State<diaryContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // height: MediaQuery.of(context).size.height.,
+      decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.vertical(top: Radius.circular(30))),
+      padding: EdgeInsets.fromLTRB(
+          10,
+          MediaQuery.of(context).size.width * 0.035,
+          MediaQuery.of(context).size.width * 0.035,
+          0),
+      // EdgeInsets.symmetric(
+      //     horizontal: MediaQuery.of(context).size.width * 0.07),
+      child: Column(
+        // mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text('2023년 04월 06일'),
+          SizedBox(
+            height: 20,
+          ),
+          Text('물통 깨뜨린 날...'),
+          SizedBox(
+            height: 20,
+          ),
+          Text('오늘 아침에 연구실에 출근하자마자 물통을 떨어뜨려서 바닥이 물범벅이 됐다. 물통 만 오천원이더 ...'),
+        ],
+      ),
+    );
+  }
+}
+
+class bottomNav extends StatefulWidget {
+  const bottomNav({super.key});
+
+  @override
+  State<bottomNav> createState() => _bottomNavState();
+}
+
+class _bottomNavState extends State<bottomNav> {
+  int current_index = 0;
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      selectedItemColor: Theme.of(context).primaryColor,
+      unselectedItemColor: Colors.grey,
+      backgroundColor: Colors.white,
+      currentIndex: current_index,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      type: BottomNavigationBarType.fixed,
+      onTap: (index) {
+        print('index test : $index');
+        setState(() {
+          current_index = index;
+        });
+      },
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.calendar_month_outlined,
+          ),
+          label: 'Calendar',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.insert_drive_file_outlined,
+          ),
+          label: 'Clip',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.circle,
+            size: 30,
+          ),
+          label: 'Somthing',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.bar_chart_rounded,
+          ),
+          label: 'Report',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.supervised_user_circle,
+          ),
+          label: 'MyPage',
+        ),
+      ],
     );
   }
 }
