ba0801e36f1477a2a759a6cec08f39ef91b2445c
zu128 <90748869+zu128@users.noreply.github.com>
2023-04-30 14:42:38 +0900

    modify myPageScreen design

--- lib/screens/mypage_screen.dart
+++ lib/screens/mypage_screen.dart
@@ -21,69 +21,85 @@ class _mypage_screState extends State<myPage> {
       // 키보드 나왔을 때 처리 화면 가려지는 거 방지용
       resizeToAvoidBottomInset: false,
       body: SafeArea(
-        child: Stack(
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 0.25,
-              // color: Theme.of(context).primaryColor,
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
-                borderRadius: const BorderRadius.vertical(
-                  bottom: Radius.circular(25),
+        child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.3,
+                // color: Theme.of(context).primaryColor,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  borderRadius: const BorderRadius.vertical(
+                    bottom: Radius.circular(25),
+                  ),
                 ),
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Icon(
-                        Icons.person_pin,
-                        color: Colors.white,
-                        size: 50,
-                      ),
-                      Text(
-                        '샤프님은',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                      Text(
-                        '일반 회원이에요',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
-            Container(
-              color: Colors.red,
-              margin: EdgeInsets.fromLTRB(
-                  MediaQuery.of(context).size.height * 0.05,
-                  MediaQuery.of(context).size.height * 0.20,
-                  MediaQuery.of(context).size.height * 0.05,
-                  MediaQuery.of(context).size.height * 0.05),
-              child: SingleChildScrollView(
+              Container(
+                margin: EdgeInsets.fromLTRB(
+                    MediaQuery.of(context).size.height * 0.05,
+                    MediaQuery.of(context).size.height * 0.025,
+                    MediaQuery.of(context).size.height * 0.05,
+                    MediaQuery.of(context).size.height * 0.05),
                 child: Column(
                   children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Icon(
+                                Icons.person_pin,
+                                color: Colors.white,
+                                size: 50,
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 10),
+                                child: Column(
+                                  children: const [
+                                    Text(
+                                      '샤프님은',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.w400,
+                                      ),
+                                    ),
+                                    Text(
+                                      '일반 회원이에요',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.w400,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                     Card(
                       child: Row(
                         children: [
-                          const recordInfo(cnt: 52, title: '총 일기'),
-                          VerticalDivider(
-                              thickness: 1,
-                              width: 1,
-                              color: Theme.of(context).dividerColor),
-                          const recordInfo(cnt: 7, title: '뭐 할까'),
-                          const recordInfo(cnt: 10, title: '뭐 하징'),
+                          recordInfo(
+                            cnt: 52,
+                            title: '총 일기',
+                          ),
+                          recordInfo(
+                            cnt: 7,
+                            title: '뭐 할까',
+                          ),
+                          recordInfo(
+                            cnt: 10,
+                            title: '뭐 하징',
+                            isRight: true,
+                          ),
                         ],
                       ),
                     ),
@@ -115,7 +131,7 @@ class _mypage_screState extends State<myPage> {
                                     '광고 제거, 다이어리 스티커 제공',
                                     style: TextStyle(
                                       color: Colors.white,
-                                      fontSize: 15,
+                                      fontSize: 12,
                                     ),
                                   ),
                                 ],
@@ -131,41 +147,22 @@ class _mypage_screState extends State<myPage> {
                     Card(
                       child: Column(
                         children: [
-                          const changeInfo(
+                          changeInfo(
                             ico: Icons.person_outline_outlined,
                             info: "아이디 변경",
                           ),
-                          Divider(
-                            thickness: 1,
-                            height: 1,
-                            color: Theme.of(context).dividerColor,
-                          ),
-                          const changeInfo(
+                          changeInfo(
                             ico: Icons.email_outlined,
                             info: "이메일변경",
                           ),
-                          Divider(
-                            thickness: 1,
-                            height: 1,
-                            color: Theme.of(context).dividerColor,
-                          ),
-                          const changeInfo(
+                          changeInfo(
                             ico: Icons.local_phone_outlined,
                             info: "휴대폰번호 변경",
                           ),
-                          Divider(
-                            thickness: 1,
-                            height: 1,
-                            color: Theme.of(context).dividerColor,
-                          ),
-                          const changeInfo(
+                          changeInfo(
                             ico: Icons.vpn_key_outlined,
                             info: "패스워드 변경",
-                          ),
-                          Divider(
-                            thickness: 1,
-                            height: 1,
-                            color: Theme.of(context).dividerColor,
+                            isBottoom: true,
                           ),
                         ],
                       ),
@@ -176,18 +173,14 @@ class _mypage_screState extends State<myPage> {
                     Card(
                       child: Column(
                         children: [
-                          const changeInfo(
+                          changeInfo(
                             ico: Icons.backup_outlined,
                             info: "데이터 백업하기",
                           ),
-                          Divider(
-                            thickness: 1,
-                            height: 1,
-                            color: Theme.of(context).dividerColor,
-                          ),
-                          const changeInfo(
+                          changeInfo(
                             ico: Icons.cloud_download_outlined,
                             info: "데이터 복원하기",
+                            isBottoom: true,
                           ),
                         ],
                       ),
@@ -197,10 +190,11 @@ class _mypage_screState extends State<myPage> {
                     ),
                     Card(
                       child: Column(
-                        children: const [
+                        children: [
                           changeInfo(
                             ico: Icons.library_books_outlined,
                             info: "데이터 내보내기",
+                            isBottoom: true,
                           ),
                         ],
                       ),
@@ -208,8 +202,8 @@ class _mypage_screState extends State<myPage> {
                   ],
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -219,18 +213,24 @@ class _mypage_screState extends State<myPage> {
 class recordInfo extends StatelessWidget {
   final int cnt;
   final String title;
-  const recordInfo({
+  bool isRight;
+  recordInfo({
     super.key,
     required this.cnt,
     required this.title,
+    this.isRight = false,
   });
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
-        decoration: const BoxDecoration(
-          color: Color.fromRGBO(255, 255, 255, 1),
+        decoration: BoxDecoration(
+          border: Border(
+              right: !isRight
+                  ? BorderSide(color: Theme.of(context).dividerColor)
+                  : BorderSide.none),
+          color: const Color.fromARGB(1, 255, 255, 255),
         ),
         padding: const EdgeInsets.all(15),
         child: Column(
@@ -238,12 +238,16 @@ class recordInfo extends StatelessWidget {
             Text(
               '$cnt',
               style: const TextStyle(
-                  color: Color.fromRGBO(124, 124, 124, 1), fontSize: 20),
+                  color: Color.fromRGBO(124, 124, 124, 1),
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600),
             ),
             Text(
               title,
               style: const TextStyle(
-                  color: Color.fromRGBO(124, 124, 124, 1), fontSize: 15),
+                color: Color.fromRGBO(124, 124, 124, 1),
+                fontSize: 15,
+              ),
             ),
           ],
         ),
@@ -255,10 +259,12 @@ class recordInfo extends StatelessWidget {
 class changeInfo extends StatelessWidget {
   final String info;
   final IconData ico;
-  const changeInfo({
+  bool isBottoom;
+  changeInfo({
     super.key,
     required this.info,
     required this.ico,
+    this.isBottoom = false,
   });
 
   @override
@@ -268,8 +274,12 @@ class changeInfo extends StatelessWidget {
         print(info);
       },
       child: Container(
-        decoration: const BoxDecoration(
-          color: Color.fromARGB(1, 255, 255, 255),
+        decoration: BoxDecoration(
+          border: Border(
+              bottom: !isBottoom
+                  ? BorderSide(color: Theme.of(context).dividerColor)
+                  : BorderSide.none),
+          color: const Color.fromARGB(1, 255, 255, 255),
         ),
         padding: const EdgeInsets.all(15),
         child: Row(
@@ -279,10 +289,13 @@ class changeInfo extends StatelessWidget {
               size: 30,
               color: const Color.fromRGBO(124, 124, 124, 1),
             ),
-            Text(
-              info,
-              style: const TextStyle(
-                  color: Color.fromRGBO(124, 124, 124, 1), fontSize: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                info,
+                style: const TextStyle(
+                    color: Color.fromRGBO(124, 124, 124, 1), fontSize: 17),
+              ),
             ),
           ],
         ),
